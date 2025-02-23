cd2a5b17c4fa41081da483db9c833a34d97da706
yasirnabil534 <yasir.nabil5343@gmail.com>
2025-02-24 04:39:17 +0600

    Commit-A001: Project initialization with nest, prisma and mongodb

---
+++ src/main.ts
@@ -0,0 +1,19 @@
+import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
+import { AppModule } from './app.module'
+
+async function bootstrap(): Promise<void> {
+  const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Hotel Management API')
+    .setDescription('The Hotel Management API description')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(3000);
+}
+bootstrap();
